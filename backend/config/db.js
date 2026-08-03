b44e8dd0e389fe5a1539937030726ddf1661b235
payalrajput09 <contact.payalrajput@gmail.com>
2026-08-03 22:15:43 +0530

    final commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,17 +2,18 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
+
         await mongoose.connect(process.env.MONGO_URI);
 
         console.log("✅ MongoDB Connected Successfully");
-    } catch (error) {
-    console.error("REGISTER ERROR:", error);
 
-    res.status(500).json({
-        success: false,
-        message: error.message
-    });
-}
+    } catch (error) {
+
+        console.error("❌ MongoDB Connection Error:", error.message);
+
+        process.exit(1);   // Server stop kar de agar DB connect na ho
+
+    }
 };
 
 module.exports = connectDB;
